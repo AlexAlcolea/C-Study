877c5878c0ad11b1ffda34f04a881422941dd556
Alex Alcolea <42592987+AlexAlcolea@users.noreply.github.com>
2022-03-14 10:38:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte x = 100;
+
+            byte n1 = 150;
+
+            int n2 = 1000;
+
+            long n3 = 2545848989L;
+
+            float n4 = 4.5f;
+
+            double n5 = 4.5;
+
+            bool status = false;
+
+            char gender = 'F';
+
+            string name = "Mary Jane";
+
+            object obj1 = "Alex";
+
+            object obj2 = 254;
+
+
+            Console.WriteLine(x);
+
+            Console.WriteLine(n1);
+
+            Console.WriteLine(n2);
+
+            Console.WriteLine(n3);
+
+            Console.WriteLine(status);
+
+            Console.WriteLine(gender);
+
+            Console.WriteLine(n4);
+
+            Console.WriteLine(n5);
+
+            Console.WriteLine(name);
+
+            Console.WriteLine(obj1);
+
+            Console.WriteLine(obj2);
+        }
+    }
+}
