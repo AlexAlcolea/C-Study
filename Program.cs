a6517c6963275661715c50e58a888979b079f708
Alex Alcolea <42592987+AlexAlcolea@users.noreply.github.com>
2022-03-16 13:00:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bhaskara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 3 + 4 * 2;
+            int n2 = (3 + 4) * 2;
+            int n3 = 17 % 3;
+            double n4 = (double)10 / 8;
+            double n5 = 10.0 / 8;
+
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+
+            //---------------------------------------------------//
+            //***Formula de Bhaskara***
+            double a = 1.0, b = -3.0, c = -4.0;
+
+            //Math.Pow = Potencialização
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+            //Math.Sqrt (Square Root) = Raiz Quadrada
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine(delta);
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+        }
+    }
+}
