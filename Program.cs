1b4d92650e54949ce4c940604bcc3f3728257cac
Alex Alcolea <42592987+AlexAlcolea@users.noreply.github.com>
2022-03-17 12:48:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o 2º numero: ");
+            int y = int.Parse(Console.ReadLine());
+
+            int total = y + x;
+
+            Console.WriteLine($"A soma de {y} + {x} é {total}");
+        }
+    }
+}
