858115777592e38ea81f96e27f09c7aed9132cde
Alex Alcolea <42592987+AlexAlcolea@users.noreply.github.com>
2022-03-17 12:49:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero: ");
+            double raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double area = (Math.Pow(raio, 2)) * 3.14159;
+
+            Console.WriteLine("A area do raio do numero digitado é: " + area.ToString("F4",CultureInfo.InvariantCulture));
+        }
+    }
+}
