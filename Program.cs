db3093004be041c176fdc2ab9f1d01ffef08626b
Alex Alcolea <42592987+AlexAlcolea@users.noreply.github.com>
2022-03-22 12:20:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 2 numeros: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int resul = 0;
+
+            if(a > b)
+            {
+                resul = a % b;
+            }
+            else
+            {
+                resul = b % a;
+            }
+
+            if(resul == 0)
+            {
+                Console.WriteLine("São Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não são Multiplos");
+            }
+
+
+        }
+    }
+}
