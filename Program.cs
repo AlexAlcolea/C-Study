4a42ee1941096f37fd29aae8633ebaa645c9c524
Alex Alcolea <42592987+AlexAlcolea@users.noreply.github.com>
2022-03-22 12:16:03 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,44 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Course8
+namespace Exercise2_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int horas;
-
-            Console.WriteLine("Digite que horas são: ");
-            horas = int.Parse(Console.ReadLine());
-
-            if(horas < 12)
-            {
-                Console.WriteLine("Bom dia!");
-            }
-            else if(horas >= 12 && horas < 18)
-            {
-                Console.WriteLine("Boa tarde!");
-            }
-            else
-            {
-                Console.WriteLine("Boa noite!");
-            }
-
-            Console.WriteLine("-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-");
-
-            int num;
-
             Console.WriteLine("Digite um numero inteiro: ");
-            num = int.Parse(Console.ReadLine());
+            int num = int.Parse(Console.ReadLine());
 
-            if(num % 2 == 0)
+            if(num > 0)
             {
-                Console.WriteLine($"O numero {num} é PAR!");
+                Console.WriteLine("POSITIVO");
+            }
+            else if(num == 0)
+            {
+                Console.WriteLine("VOCÊ DIGITIOU 0");
             }
             else
             {
-                Console.WriteLine($"O numero {num} é IMPAR!");
+                Console.WriteLine("NEGATIVO");
             }
         }
     }
