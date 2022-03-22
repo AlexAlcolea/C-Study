a85fa47f4196e017c1a8fb64cf3d911b51cf3c42
Alex Alcolea <42592987+AlexAlcolea@users.noreply.github.com>
2022-03-22 12:18:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero inteiro: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if(num % 2 == 0)
+            {
+                Console.WriteLine("Você digitou um numero PAR!");
+            }
+            else
+            {
+                Console.WriteLine("Você digitou um numero IMPAR!");
+            }
+        }
+    }
+}
